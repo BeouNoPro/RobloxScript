5983d99c026a5401d2b26b3e6c02d1af057e574c
BeouNoPro <99521796+BeouNoPro@users.noreply.github.com>
2022-02-12 12:15:50 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,8 +1,15 @@
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
-loadstring(game:HttpGet"https://raw.githubusercontent.com/vinhuchi/BloxFruit/main/Islandv2.lua")()
+
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/vinhuchi/BloxFruit/main/Islandv2.lua")()
+
 elseif game.PlaceId == 6299805723 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KiJinGaming/FreeScript/main/KJHub.lua"))()
-else
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KiJinGaming/FreeScript/main/KJHub.lua"))()
+
+elseif game.PlaceId == 2377868063 then
+
+    loadstring(game:HttpGet("http://gameovers.net/Scripts/Free/HitboxExpander/main.lua", true))()    
+    else
     game.Players.LocalPlayer:kick("Nhu Cac") wait(1)
     game:Shutdown()
 end
