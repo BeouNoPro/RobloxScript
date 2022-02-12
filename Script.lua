bd8ad87895add690db5146df71c7be0155988856
BeouNoPro <99521796+BeouNoPro@users.noreply.github.com>
2022-02-12 11:39:06 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,9 +1,9 @@
 local PlaceId = game.PlaceId
 
-if PlaceId == 2753915549
+elseif PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
      
 loadstring(game:HttpGet"https://raw.githubusercontent.com/vinhuchi/BloxFruit/main/Islandv2.lua")()
-elseif PlaceId == 6299805723
+elseif PlaceId == 6299805723 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KiJinGaming/FreeScript/main/KJHub.lua"))()
 else
     game.Players.LocalPlayer:kick("NoN")
